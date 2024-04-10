298d91b7b0433e67ef1c4aaddb722df08e786806
Curtis-Edwards <99354904+Curtis-Edwards@users.noreply.github.com>
2024-04-10 18:37:31 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -27,61 +27,75 @@ final class Main {
     * @param args No args will be used
     */
     public static void main(String[] args) {
+        // MultipleStringLiterals & MagicNumbers
+        final String statusString = "Status:";
+        final String newSpeedString = "New speed: ";
+        final String newColorString = "New color: ";
+        final String changingColorString = "Changing color:";
+        final int fourConst = 4;
+        final int twoHundredEightyFourConst = 284;
+        final int tenConst = 10;
+        final int threeHundredTwentyConst = 320;
+        final int oneHundredConst = 100;
+        final int fortyFiveConst = 45;
+        final int fiveConst = 5;
+        
         System.out.println("Created Ford focus");
-        Vehicle fordFocus = new Vehicle("white", "B40-C4RR0", 4, 284);
+        final Vehicle fordFocus = new Vehicle("white", "B40-C4RR0",
+            fourConst, twoHundredEightyFourConst);
 
-        System.out.println("Status:");
+        System.out.println(statusString);
         fordFocus.status();
 
         System.out.println("Accelerating, 10 of power for 10 sec.");
-        fordFocus.accelerate(10, 10);
-        System.out.println("New speed: " + fordFocus.getSpeed());
+        fordFocus.accelerate(tenConst, tenConst);
+        System.out.println(newSpeedString + fordFocus.getSpeed());
 
         System.out.println("Changing colour to black:");
         fordFocus.setColor("black");
-        System.out.println("New color: " + fordFocus.getColor());
+        System.out.println(newColorString + fordFocus.getColor());
 
         System.out.println("");
 
         // New Ferrari
         System.out.println("Created a Ferrari");
-        Vehicle ferrari = new Vehicle("red", "VHD-35HU", 2, 320);
+        final Vehicle ferrari = new Vehicle("red", "VHD-35HU", 2, threeHundredTwentyConst);
 
-        System.out.println("Status:");
+        System.out.println(statusString);
         ferrari.status();
 
         System.out.println("Accelerating, 100 of power for 4 sec.");
-        ferrari.accelerate(100, 4);
-        System.out.println("New speed: " + ferrari.getSpeed());
+        ferrari.accelerate(oneHundredConst, fourConst);
+        System.out.println(newSpeedString + ferrari.getSpeed());
 
         System.out.println("Breaking, 10 of power for 10 sec.");
-        ferrari.brake(10, 10);
-        System.out.println("New speed: " + ferrari.getSpeed());
+        ferrari.brake(tenConst, tenConst);
+        System.out.println(newSpeedString + ferrari.getSpeed());
 
-        System.out.println("Changing color:");
+        System.out.println(changingColorString);
         ferrari.setColor("racing red!");
-        System.out.println("New color: " + ferrari.getColor());
+        System.out.println(newColorString + ferrari.getColor());
 
         System.out.println("");
 
         // New Citroen Ami
         System.out.println("Created a Citroen Ami");
-        Vehicle citroenAmi = new Vehicle("blue", "FS-695-DQ", 2, 45);
+        final Vehicle citroenAmi = new Vehicle("blue", "FS-695-DQ", fiveConst, fortyFiveConst);
 
-        System.out.println("Status:");
+        System.out.println(statusString);
         citroenAmi.status();
 
         System.out.println("Accelerating, 5 of power for 10 sec.");
-        citroenAmi.accelerate(5, 10);
-        System.out.println("New speed: " + citroenAmi.getSpeed());
+        citroenAmi.accelerate(fiveConst, tenConst);
+        System.out.println(newSpeedString + citroenAmi.getSpeed());
 
         System.out.println("Breaking, 2 of power for 10 sec.");
-        citroenAmi.brake(2, 10);
-        System.out.println("New speed: " + citroenAmi.getSpeed());
+        citroenAmi.brake(fiveConst, tenConst);
+        System.out.println(newSpeedString + citroenAmi.getSpeed());
 
-        System.out.println("Changing color:");
+        System.out.println(changingColorString);
         citroenAmi.setColor("purple");
-        System.out.println("New color: " + citroenAmi.getColor());
+        System.out.println(newColorString + citroenAmi.getColor());
 
         System.out.println("\nDone.");
     }
